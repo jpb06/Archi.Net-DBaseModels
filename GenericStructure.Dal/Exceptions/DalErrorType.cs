a1f7bd737c0e1b7c80c1e1e00d6a1746df03f08a
jpb <jpb.06@outlook.com>
2017-09-30 15:09:15 +0200

    First sketch for unified exceptions handling

---
+++ GenericStructure.Dal/Exceptions/DalErrorType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericStructure.Dal.Exceptions
+{
+    // [1.000.000 - 4.999.999] = 20.000 units of code having 200 defined errors each
+    public enum DalErrorType
+    {
+        #region SalesService.CreateArticle
+
+        #endregion
+    }
+}
---
+++ GenericStructure.Dal/Exceptions/DalException.cs
@@ -0,0 +1,14 @@
+﻿using GenericStructure.Framework.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericStructure.Dal.Exceptions
+{
+    public class DalException : BaseException
+    {
+        public DalException() : base("") { }
+    }
+}
