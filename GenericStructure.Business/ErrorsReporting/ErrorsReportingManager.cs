30a1b1304bc819e6b6186b5d4c2166639b8b5b01
jpb <jpb.06@outlook.com>
2017-10-08 17:35:00 +0200

    Business layer first steps for errors reporting

---
+++ GenericStructure.Business/ErrorsReporting/ErrorsReportingManager.cs
@@ -0,0 +1,34 @@
+﻿using GenericStructure.Dal.Manipulation.Services.ErrorsReporting;
+using GenericStructure.Dal.Manipulation.Services.ErrorsReporting.Contracts;
+using GenericStructure.Dal.Models.ErrorsReporting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericStructure.Business.ErrorsReporting
+{
+    public class ErrorsReportingManager
+    {
+        private IErrorsReportingService service;
+
+        public ErrorsReportingManager(IErrorsReportingService service) 
+        {
+            this.service = service;
+        }
+
+        public void LogError(Exception exception, AssemblyName AssemblyInfos, string errorCodeFullyQualifiedName) 
+        {
+            string applicationName = AssemblyInfos.Name;
+            string applicationVersion = AssemblyInfos.Version.ToString();
+
+            ErrorReportApplication application = this.service.GetApplication(applicationName, applicationVersion);
+            if(application == null)
+                application = this.service.CreateApplication(applicationName, applicationVersion);
+
+            this.service.LogException(application.Id, exception);
+        }
+    }
+}
